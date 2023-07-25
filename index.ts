3b28dbe2809db56da199deccd0687ddb63f207b7
Muammer Akca <muammerakca10@gmail.com>
2023-07-25 09:54:38 +0300

    Changed .gitignore

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log("object");
+import express from 'express';
+import {google} from 'googleapis'
+
+const app = express();
+
+
+
+app.use((req: express.Request, res: express.Response, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+  });
+
+app.get('/', async (req : express.Request, res : express.Response) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile : "credentials.json",
+        scopes : "https://www.googleapis.com/auth/spreadsheets"
+    });
+
+    const client = await auth.getClient()
+    console.log(client);
+
+    const googleSheets = google.sheets({version : "v4" , auth : auth})  /// HATA VEREBILIR auth tipinden dolayi
+
+    const metaData = await googleSheets.spreadsheets.get({
+        auth,
+
+    })
+})
+
+
+app.listen(6000, ()=>{
+    console.log("Listening 6000");
+} )
